1698c81633ff29ee854fbd024e2dcef40e522b2b
Robert Kalescky <rkalescky@smu.edu>
2025-01-21 09:44:59 -0600

    Rename parallel to distributed.

--- scripts/qr.py
+++ scripts/qr.py
@@ -4,6 +4,6 @@ def make_qr(url, image):
     qr = segno.make_qr(url)
     qr.save(image, scale=16)
 
-make_qr("https://southernmethodistuniversity.github.io/parallel_python/intro.html", 
-        "../docs/parallel_python_qr.png")
+make_qr("https://southernmethodistuniversity.github.io/distributed_python/intro.html", 
+        "../docs/distributed_python_qr.png")
 
